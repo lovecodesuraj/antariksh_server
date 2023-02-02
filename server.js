1f9ae026a63d4d21b755c9725819b7e0ca9db922
lovecodesuraj <chaudharysuraj2252@gmail.com>
2023-02-02 11:19:20 +0530

    server_deploy_1.0

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from 'cors'
+
+import connectToDb from "./db.js"; 
+
+import postRoutes from './routes/astrons.js' 
+
+// import multer from "multer";
+// const upload = multer({dest:"uploads/"});
+
+
+
+
+
+const app=express();
+
+
+
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({ limit:"30mb",extended: true }));
+app.use(cors());
+
+connectToDb();
+
+app.use('/astrons',postRoutes);
+
+
+app.listen(5000,function(res){
+    console.log("server is running at port 5000");
+})
+
+
+
+
+// refresh token :  1//04u07Y-YrPNLBCgYIARAAGAQSNwF-L9Irp6VNIBbnfIaSsBf1kwzPiLJnvZqwFHWGk4TJQqyDW1N8o90CXNEiDgrimqM8UBqQmH0
